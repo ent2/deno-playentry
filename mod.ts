9d2e494695cf6b7bf25467091179ba98814a3a0d
Gnlow <entrydark@gmail.com>
2020-10-22 14:27:39 +0900

    export

--- mod.ts
+++ mod.ts
@@ -1,14 +1,3 @@
-import { basicFetch } from "./src/util.ts"
-import User from "./src/User.ts"
-
-export const getUserByUsername = async (username: string) => {
-    const res = await basicFetch(`getUserByUsername/${username}`)
-    let info = Object.assign(res, {
-        id: res._id,
-        avatarURL: 
-            res.avatarImage 
-                ? `https://playentry.org/uploads/profile/${res._id.substring(0, 2)}/${res._id.substring(2, 4)}/avatar_${res._id}.png`
-                : "https://playentry.org/img/assets/avatar_img.png"
-    })
-    return new User(info)
-}
+export * from "./src/util.ts"
+export {default as User} from "./src/User.ts"
+export {default as Project} from "./src/Project.ts"
