ee232b6f0e3d243577893fdf5051e10b5cd34280
Gnlow <entrydark@gmail.com>
2020-10-22 17:53:23 +0900

    handling no-user case

--- src/util.ts
+++ src/util.ts
@@ -7,15 +7,17 @@ export async function basicFetch (urlStr: string, params?: Record<string, string
     return await fetch(url).then(res => res.json())
 }
 
-/** 유저의 닉네임(아이디)로 정보를 불러온다. */
+/** 유저의 닉네임(아이디)으로 정보를 불러온다. 유저가 존재하지 않는다면 `undefined`를 반환한다. */
 export async function getUserByUsername (username: string) {
     const res = await basicFetch(`getUserByUsername/${username}`)
-    let info = Object.assign(res, {
-        id: res._id,
-        avatarURL: 
-            res.avatarImage 
-                ? `https://playentry.org/uploads/profile/${res._id.substring(0, 2)}/${res._id.substring(2, 4)}/avatar_${res._id}.png`
-                : "https://playentry.org/img/assets/avatar_img.png"
-    })
-    return new User(info)
+    if (res) {
+        let info = Object.assign(res, {
+            id: res._id,
+            avatarURL: 
+                res.avatarImage 
+                    ? `https://playentry.org/uploads/profile/${res._id.substring(0, 2)}/${res._id.substring(2, 4)}/avatar_${res._id}.png`
+                    : "https://playentry.org/img/assets/avatar_img.png"
+        })
+        return new User(info)
+    }
 }
